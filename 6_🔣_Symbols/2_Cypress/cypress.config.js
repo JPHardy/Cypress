91ff2a1ba530cedb00cf340c08ed1e4157963ab5
Erdem <rifaterdemsahin@gmail.com>
2025-04-01 14:41:25 +0000

    nuked and back to what i see in the ports

--- 6_🔣_Symbols/2_Cypress/cypress.config.js
+++ 6_🔣_Symbols/2_Cypress/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://verbose-guacamole-6w5jqx7v65cgjq-3000.app.github.dev', // Replace with actual URL
+    baseUrl: 'verbose-guacamole-6w5jqx7v65cgjq-34537.app.github.dev', // Replace with actual URL
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
     chromeWebSecurity: false, // Disables web security (use cautiously)
